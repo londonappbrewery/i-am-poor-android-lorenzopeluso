614619ac78110c9947964b74e3d57e8c3f1ae6dd
Lorenzo <pelusolorenzo@gmail.com>
2017-11-01 22:04:55 +0100

    Challenge made.

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Call the superclass onCreate method
